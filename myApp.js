15e7401972fadb946408b97886410527fcf0b716
HIEU Le Trung <hieuletrung102@gmail.com>
2018-12-09 22:30:19 +0700

    Finish task8 Find Edit Save

--- myApp.js
+++ myApp.js
@@ -190,8 +190,14 @@ var findPersonById = function(personId, done) {
 
 var findEditThenSave = function(personId, done) {
   var foodToAdd = 'hamburger';
-  
-  done(null/*, data*/);
+  Person.findById(personId, (err, person) => {
+    if (err) return (done(err))
+    person['favoriteFoods'].push(foodToAdd)
+    person.save((err, data) => {
+      if (err) return done(err)
+      done(null, data)
+    })
+  })
 };
 
 /** 9) New Update : Use `findOneAndUpdate()` */
